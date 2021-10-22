8d7f373a36b4f3efc1dff6b117e254fabb186e42
QiuOOGan <ganqiu@ucdavis.edu>
2021-10-22 12:34:31 -0500

    map search markers

---
+++ src/components/Autocomplete2.js
@@ -0,0 +1,77 @@
+// Autocomplete2.js
+import React, { Component } from 'react';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  position: relative;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  padding: 20px;
+  text-align:center;
+`;
+
+class AutoComplete2 extends Component {
+    constructor(props) {
+        super(props);
+        this.clearSearchBox = this.clearSearchBox.bind(this);
+    }
+
+    componentDidMount({ map, mapApi } = this.props) {
+        const options = {
+            // restrict your search to a specific type of result
+            types: ['address'],
+            // restrict your search to a specific country, or an array of countries
+            // componentRestrictions: { country: ['gb', 'us'] },
+        };
+        this.autoComplete = new mapApi.places.Autocomplete(
+            this.searchInput,
+            options,
+        );
+        this.autoComplete.addListener('place_changed', this.onPlaceChanged);
+        this.autoComplete.bindTo('bounds', map);
+    }
+
+    componentWillUnmount({ mapApi } = this.props) {
+        mapApi.event.clearInstanceListeners(this.searchInput);
+    }
+
+    onPlaceChanged = ({ map, addplace } = this.props) => {
+        const place = this.autoComplete.getPlace();
+
+        if (!place.geometry) return;
+        if (place.geometry.viewport) {
+            map.fitBounds(place.geometry.viewport);
+        } else {
+            map.setCenter(place.geometry.location);
+            map.setZoom(17);
+        }
+
+        addplace(place);
+        this.searchInput.blur();
+    };
+
+    clearSearchBox() {
+        this.searchInput.value = '';
+    }
+
+    render() {
+        return (
+            <Wrapper>
+                <div> 
+                  <input
+                      className="search-input"
+                      ref={(ref) => {
+                          this.searchInput = ref;
+                      }}
+                      type="text"
+                      onFocus={this.clearSearchBox}
+                      placeholder="Enter a location"
+                  />
+                </div>
+            </Wrapper>
+        );
+    }
+}
+
+export default AutoComplete2;
---
+++ src/components/MyGoogleMap.js
@@ -0,0 +1,261 @@
+// MyGoogleMaps.js
+import React, { Component } from 'react';
+
+import GoogleMapReact from 'google-map-react';
+import { fitBounds } from 'google-map-react';
+
+import styled from 'styled-components';
+
+import AutoComplete from './Autocomplete';
+import Marker from './Markers';
+
+
+const Wrapper = styled.main`
+  width: 100%;
+  height: 100%;
+`;
+
+class MyGoogleMap extends Component {
+
+
+    state = {
+        mapApiLoaded: false,
+        mapInstance: null,
+        mapApi: null,
+        geoCoder: null,
+        center: [],
+        zoom: 9,
+        draggable: true,
+        places: []
+
+    };
+
+    componentWillMount() {
+        this.setCurrentLocation();
+    }
+
+
+    onMarkerInteraction = (childKey, childProps, mouse) => {
+        this.setState({
+            draggable: false,
+            lat: mouse.lat,
+            lng: mouse.lng
+        });
+    }
+    onMarkerInteractionMouseUp = (childKey, childProps, mouse) => {
+        this.setState({ draggable: true });
+        this._generateAddress();
+    }
+
+    _onChange = ({ center, zoom }) => {
+        console.log("center:", center);
+        this.setState({
+            center: center,
+            zoom: zoom,
+        });
+
+    }
+
+    rad2degr(rad) { return rad * 180 / Math.PI; }
+    degr2rad(degr) { return degr * Math.PI / 180; }
+    getLatLngCenter(latLngInDegr) {
+      var LATIDX = 0;
+      var LNGIDX = 1;
+      var sumX = 0;
+      var sumY = 0;
+      var sumZ = 0;
+  
+      for (var i=0; i<latLngInDegr.length; i++) {
+          var lat = this.degr2rad(latLngInDegr[i][LATIDX]);
+          var lng = this.degr2rad(latLngInDegr[i][LNGIDX]);
+          // sum of cartesian coordinates
+          sumX += Math.cos(lat) * Math.cos(lng);
+          sumY += Math.cos(lat) * Math.sin(lng);
+          sumZ += Math.sin(lat);
+      }
+  
+      var avgX = sumX / latLngInDegr.length;
+      var avgY = sumY / latLngInDegr.length;
+      var avgZ = sumZ / latLngInDegr.length;
+  
+      // convert average x, y, z coordinate to latitude and longtitude
+      var lng = Math.atan2(avgY, avgX);
+      var hyp = Math.sqrt(avgX * avgX + avgY * avgY);
+      var lat = Math.atan2(avgZ, hyp);
+  
+      return [this.rad2degr(lat), this.rad2degr(lng)];
+  }
+
+
+    _onClick = (value) => {
+        this.setState({
+            lat: value.lat,
+            lng: value.lng
+        });
+    }
+
+    apiHasLoaded = (map, maps) => {
+        this.setState({
+            mapApiLoaded: true,
+            mapInstance: map,
+            mapApi: maps,
+        });
+
+        this._generateAddress();
+    };
+
+    addPlace = (place) => {
+
+        // const newPlace = 
+        //   {
+        //     place: place,
+        //     lat: place.geometry.location.lat(),
+        //     lng: place.geometry.location.lng(),
+        //     address: this._generateAddress(place.geometry.location.lat(), place.geometry.location.lng())
+        //   }
+
+        // this.setState(prevState => ({
+        //   places: [...prevState.places, newPlace]
+        // }));
+        this._generateAddress(place);
+    };
+
+    _generateAddress(place) {
+        const {
+            mapApi
+        } = this.state;
+
+        if (!place){
+          return;
+        }
+
+        const geocoder = new mapApi.Geocoder;
+        const lat = place.geometry.location.lat();
+        const lng = place.geometry.location.lng();
+
+        geocoder.geocode({ 'location': { lat: lat, lng: lng } }, (results, status) => {
+            console.log(results);
+            console.log(status);
+            if (status === 'OK') {
+                if (results[0]) {
+                    this.zoom = 12;
+                    const newPlace = 
+                      {
+                        place: place,
+                        lat: place.geometry.location.lat(),
+                        lng: place.geometry.location.lng(),
+                        address: results[0].formatted_address
+                      }
+                      this.setState(prevState => ({
+                        places: [...prevState.places, newPlace]
+                      }));
+                      console.log("places:", this.state.places);   
+                } else {
+                    window.alert('No results found');
+                }
+            } else {
+                window.alert('Geocoder failed due to: ' + status);
+            }
+
+        });
+    }
+
+    // Get Current Location Coordinates
+    setCurrentLocation() {
+
+      this.setState({
+        center: [42.0494669, -87.688195],
+        lat: 42.0494669,
+        lng: -87.688195
+      })
+        // if ('geolocation' in navigator) {
+        //     navigator.geolocation.getCurrentPosition((position) => {
+        //         this.setState({
+        //             center: [position.coords.latitude, position.coords.longitude],
+        //             lat: position.coords.latitude,
+        //             lng: position.coords.longitude
+        //         });
+        //     });
+        // }
+    }
+
+    addressRenderer() {
+      return (
+        <>
+          {this.state.places.map((place, ind) => (
+            <div className="map-details">Person {ind + 1}: <span>{place.address}</span></div>
+          ))}
+        </>
+      )
+    }
+
+    markerRenderer(){
+      return (
+          this.state.places.map((place, ind) => (
+            <Marker
+              text={place.address}
+              lat={place.lat}
+              lng={place.lng}
+            />  
+          ))
+      )
+    }
+
+    render() {
+        const {
+            places, mapApiLoaded, mapInstance, mapApi,
+        } = this.state;
+
+
+        return (
+            <Wrapper>
+                {mapApiLoaded && (
+                  <div>
+                    <div>
+                        <AutoComplete map={mapInstance} mapApi={mapApi} addplace={this.addPlace} />
+                    </div>
+                  </div>
+                )}
+                <GoogleMapReact
+                    center={this.state.center}
+                    zoom={this.state.zoom}
+                    draggable={this.state.draggable}
+                    onChange={this._onChange}
+                    onChildMouseDown={this.onMarkerInteraction}
+                    onChildMouseUp={this.onMarkerInteractionMouseUp}
+                    onChildMouseMove={this.onMarkerInteraction}
+                    onChildClick={() => console.log('child click')}
+                    onClick={this._onClick}
+                    bootstrapURLKeys={{
+                        key: 'AIzaSyB3L47aJjmVQz2c0hoDP6WYD-qRaNKdnQU',
+                        libraries: ['places', 'geometry'],
+                    }}
+                    yesIWantToUseGoogleMapApiInternals
+                    onGoogleApiLoaded={({ map, maps }) => this.apiHasLoaded(map, maps)}
+                >
+
+                {/* <Marker
+                    text={this.state.address}
+                    lat={this.state.lat}
+                    lng={this.state.lng}
+                /> */}
+
+                {this.markerRenderer()}
+
+
+                </GoogleMapReact>
+
+                <div className="info-wrapper">
+                    {/* <div className="map-details">Latitude: <span>{this.state.lat}</span>, Longitude: <span>{this.state.lng}</span></div>
+                    <div className="map-details">Zoom: <span>{this.state.zoom}</span></div> */}
+                    {this.addressRenderer()}
+                    {/* <div className="map-details">Address: <span>{this.state.address}</span></div> */}
+                </div>
+
+
+            </Wrapper >
+        );
+    }
+}
+
+export default MyGoogleMap;
