3a2e43150488124afae3e241a3bea128829ae1a6
QiuOOGan <ganqiu@ucdavis.edu>
2021-10-24 14:59:05 -0500

    add a test

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
 
   return (
     <div className="App">
-      <h1> Where2Meet </h1>
+      <h1 data-testid = "AppTitle">Where2Meet</h1>
       {/* <MainPostTopicComponent/> */}
       <div className="main-wrapper">
         <MyGoogleMap />
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,10 @@
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe("Title Test", () => {
+  it("Rendered Title", () => {
+    const {getByTestId} = render(<App/>);
+    const title = getByTestId("AppTitle");
+    expect(title.innerHTML).toBe("Where2Meet");
+  })
+})
