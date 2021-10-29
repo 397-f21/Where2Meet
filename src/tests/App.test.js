585f4a3c2fe3f113a716b9439a44ca5516d83e67
rgoodman22 <goodman.rob24@gmail.com>
2021-10-28 22:33:56 -0500

    moving app.test to tests dir

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,9 +1,9 @@
 import { render } from '@testing-library/react';
-import App from './App';
+import App from '../App';
 
 describe("Title Test", () => {
   it("Rendered Title", () => {
-    const {getByTestId} = render(<App/>);
+    const { getByTestId } = render(<App />);
     const title = getByTestId("AppTitle");
     expect(title.innerHTML).toBe("Where2Meet");
   })
