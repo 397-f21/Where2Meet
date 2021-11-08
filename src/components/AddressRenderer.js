1c8492cb1643e640b0bc62e4da5e4ebd02c2403e
rgoodman22 <goodman.rob24@gmail.com>
2021-11-08 13:41:55 -0600

    solo changes ~ ETA w/ transit type select; improved styling; removing meeting state when new place added

--- src/components/AddressRenderer.js
+++ src/components/AddressRenderer.js
@@ -1,5 +1,9 @@
-import {React, useState} from 'react';
+import React, { useState, useEffect } from 'react';
 import './AddressSidebar.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Select from 'react-select';
+import makeAnimated from 'react-select/animated';
+
 
 
 
@@ -7,24 +11,46 @@ export const AddressRenderer = ({ places, setPlaces, meetState, mapState }) => {
 
     const [etaIndex, setIndex] = useState(-1);
     const [eta, setETA] = useState("");
+    const [type, setType] = useState("DRIVING");
+    const animatedComponents = makeAnimated();
+    const typeOptions = [
+        { value: 'DRIVING', label: 'Driving' },
+        { value: 'WALKING', label: 'Walking' },
+        { value: 'BICYCLING', label: 'Biking' },
+        { value: 'TRANSIT', label: 'Transit' }
+    ];
 
-    const getEta = (origin, ind) => {
-        if(!mapState.mapApiLoaded) {
+    useEffect(() => {
+        setIndex(-1);
+        setETA("");
+    }, [places]);
+
+
+    const getEta = (origin, ind, transitType) => {
+
+        if (!mapState.mapApiLoaded) {
+            return;
+        }
+        else if (ind === etaIndex && transitType['value'] === type['value']) {
+            console.log(transitType, type);
+
+            setIndex(-1);
             return;
         }
         const service = new window.google.maps.DistanceMatrixService();
         let loc = new window.google.maps.LatLng(origin.lat, origin.lng);
-        let destination = new window.google.maps.LatLng(meetState.meet_loc_lat,meetState.meet_loc_lng)
+        let destination = new window.google.maps.LatLng(meetState.meet_loc_lat, meetState.meet_loc_lng)
         service.getDistanceMatrix(
             {
-              origins: [loc],
-              destinations: [destination],
-              travelMode: 'WALKING'
-            }, (response,status) => {
+                origins: [loc],
+                destinations: [destination],
+                travelMode: transitType['value'],
+            }, (response, status) => {
                 setETA(response.rows[0].elements[0].duration.text);
-                console.log(response,status);
+                console.log(response, status);
             }
         );
+        setType(transitType);
         setIndex(ind)
     }
     return (<>
@@ -33,16 +59,51 @@ export const AddressRenderer = ({ places, setPlaces, meetState, mapState }) => {
             <ul data-testid="addressList">
                 {places.map((place, ind) => (
                     <>
-                        <li data-testid= "Address" className="card-text" align="left" id={`${place.address.split(' ')[1]}${ind}`} key={ind}>
-                            <b>Person {ind + 1}:</b> <span style={{textOverflow: 'ellipsis'}}>{place.address}</span>
+                        <li data-testid="Address" className="card-text" id={`${place.address.split(' ')[1]}${ind}`} key={ind}>
+                            <div className='d-flex flex-nowrap'>
+                                <div className='p-1 me-auto text-truncate align-self-center'>
+                                    <b>Person {ind + 1}:</b>
+                                    <span className=''>{' ' + place.address}</span>
+                                </div>
+                                <div className='p-1' style={{ marginRight: '5px' }}>
+                                    <div className="btn-group" role="group" aria-label="Button group with nested dropdown">
+                                        {meetState.meet_address ?
+                                            <button
+                                                data-test-id="TravelTimeButton"
+                                                type="button"
+                                                class="btn btn-primary dropdown-toggle"
+                                                data-toggle="dropdown"
+                                                aria-haspopup="true"
+                                                aria-expanded="false"
+                                                onClick={() => getEta(place, ind, type)}>
+                                                ETA↓
+                                            </button>
+                                            : <> </>}
+                                        <button type="button" data-testid="DeleteButton" className="btn btn-secondary" onClick={() => del(places, setPlaces, ind)}>X</button>
+                                    </div>
+                                </div>
+                            </div>
+                            {etaIndex === ind ?
+                                <>
+                                    <div className='d-flex'>
+                                        <Select
+                                            className="p-1 align-self-center"
+                                            closeMenuOnSelect={false}
+                                            components={animatedComponents}
+                                            defaultValue={type}
+                                            options={typeOptions}
+                                            onChange={(selected) => {
+                                                getEta(places[etaIndex], etaIndex, selected);
+                                            }}
+                                        >
+                                        </Select>
+                                        <span className="p-1 align-self-center">{eta !== "" ? `Estimated ${eta} to center` : 'Select a transit type'}</span>
+                                    </div>
+                                </>
+                                : null}
 
-                            <span style={{width: '50px'}}>
-                                <button data-testid="DeleteButton" className="btn btn-secondary" className="button" onClick={() => del(places, setPlaces, ind)}>X</button>
-                                { meetState.meet_address ? <button data-testid="TravelTimeButton" className="btn btn-secondary" className="button" onClick={() =>getEta(place, ind)}>ETA↓</button> : <> </>}
-                            </span>
                         </li>
 
-                        { etaIndex === ind ? <li> {eta} </li> : <> </>}
                     </>
                 ))}
             </ul>
@@ -50,9 +111,9 @@ export const AddressRenderer = ({ places, setPlaces, meetState, mapState }) => {
     </>)
 };
 
-function del (places, setPlaces, ind) {
+function del(places, setPlaces, ind) {
     console.log("Del pressed!");
-    setPlaces([...places.filter(function(val, i) {
+    setPlaces([...places.filter(function (val, i) {
         return i !== ind
     })])
 }
@@ -67,7 +128,7 @@ export const Wrapper = () => {
     ];
     const [place, setPlaces] = useState(temp);
     return (
-            <AddressRenderer places = {place} setPlaces = {setPlaces}/>
+        <AddressRenderer places={place} setPlaces={setPlaces} />
     )
 };
 
--- src/components/AddressSidebar.js
+++ src/components/AddressSidebar.js
@@ -3,6 +3,7 @@ import CalculateCenter from "./CalculateCenter";
 import MeetingLocation from "./MeetingLocation";
 import AddressRenderer from './AddressRenderer';
 import NearbySearch from "./NearbySearch";
+import { useEffect } from "react";
 
 export default function AddressSidebar({ mapState, meetState, setMeetState, places, setPlaces, recoms, setRecoms, setCenter, setZoom }) {
     const recenter = (lat, lng) => {
@@ -10,38 +11,52 @@ export default function AddressSidebar({ mapState, meetState, setMeetState, plac
         setCenter([lat, lng]);
     }
 
+    useEffect(() => {
+        setMeetState({
+            meet_loc_lat: null,
+            meet_loc_lng: null,
+            meet_address: null,
+            meet_types: null
+        })
+    }, [places])
+
     if (mapState.mapApiLoaded) {
         return (
             <>
-                {places.length === 0 ? null : 
-                    <div className="card m-2 p-2 scroll">
+                {places.length === 0 ? null :
+                    <>
                         <h5 className="card-title"><img className="redPin"
-                                                        src="https://icon-library.com/images/pin-icon-png/pin-icon-png-9.jpg"
-                                                        alt="new"/>People:
+                            src="https://icon-library.com/images/pin-icon-png/pin-icon-png-9.jpg"
+                            alt="new" />People:
                         </h5>
-                        <AddressRenderer places={places} setPlaces={setPlaces} meetState = {meetState} mapState={mapState} />
-                    </div>
+                        <div className="card-lg m-2 p-2 scroll">
+                            <AddressRenderer places={places} setPlaces={setPlaces} meetState={meetState} mapState={mapState} />
+                        </div>
+                    </>
                 }
                 <div>
                     <CalculateCenter places={places} mapState={mapState} setMeetState={setMeetState}
                         setCenter={setCenter} />
                 </div>
-                <MeetingLocation meetState={meetState}/>
-                {!meetState?.meet_address || meetState?.meet_types?.includes('plus_code') ? null : 
-                    <div className="card m-2 p-2 scroll customizedCard">
-                        <NearbySearch meetState={meetState}  setRecoms={setRecoms}
-                                    keyword={''} mapState={mapState}>
-                        </NearbySearch>
-                        
-                        <div className="row justify-content-md-center">
-                            {recoms.map((recom) => (
-                                <div className="card m-1 p-1 col-md-5 col-sm-12 bg-info" onClick={() => {recenter(recom.lat(), recom.lng())}}>
-                                    {recom.name}
-                                </div>
-                            ))}
-                        </div>
+                {!meetState?.meet_address || meetState?.meet_types?.includes('plus_code') ? null :
+                    <>
+                        <MeetingLocation meetState={meetState} />
+                        <div className="card m-2 p-2 scroll customizedCard">
+                            <NearbySearch meetState={meetState} setRecoms={setRecoms}
+                                keyword={''} mapState={mapState}>
+                            </NearbySearch>
 
-                    </div>
+                            <div className="row justify-content-md-center">
+                                {recoms.map((recom) => (
+                                    <div className="card m-1 p-1 col-md-5 col-sm-12 bg-info" onClick={() => { recenter(recom.lat(), recom.lng()) }}>
+                                        {recom.name}
+                                    </div>
+                                ))}
+                            </div>
+
+                        </div>
+                    </>
+
                 }
             </>
         )
