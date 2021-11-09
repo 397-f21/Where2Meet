bcaa8f443efe0fb4024dbe3eadc959293a69e941
rtl9148 <robertloza2021@u.northwestern.edu>
2021-11-08 20:07:17 -0600

    Robert Loza test finished finally

--- src/tests/CalculateCenter.test.js
+++ src/tests/CalculateCenter.test.js
@@ -1,5 +1,6 @@
 import { render } from "@testing-library/react";
 import CalculateCenter from  '../components/CalculateCenter';
+import {getLatLngCenter} from '../utils/utils';
 
 describe("Calculate Center Test", () => {
     it("Calculate Button shows up if two or more locations entered", () => {
@@ -16,24 +17,61 @@ describe("Calculate Center Test", () => {
 
     it("For any two locations, check that the Midpoint is equidistant from those locations", () => {
       //Code Here
-       const {queryByTestId} = render(<CalculateCenter places={
+       const places =[
         {
-          meet_loc_lat: 10,
-          meet_loc_lng: 20,
-          meet_address: "855 Hinman Ave.",
+          lat: 10,
+          lng: 20,
+          address: "855 Hinman Ave.",
           meet_types: ["premise"]
         },
         {
-          meet_loc_lat: 30,
-          meet_loc_lng: 50,
-          meet_address:"123 my address Cir.",
+          lat: 30,
+          lng: 50,
+          address:"123 my address Cir.",
           meet_types: ["premise"]
-        }
-       }/>);
-       
+        }];
+      const R = 6371e3; // metres
+      const my_mdpt = getLatLngCenter(places)
+
+      const φ1_1 = my_mdpt[0] * Math.PI/180; 
+      const φ2_1 = places[0].lat * Math.PI/180;
+      const Δφ_1 = (places[0].lat-my_mdpt[0]) * Math.PI/180;
+      const Δλ_1 = (places[0].lng-my_mdpt[1]) * Math.PI/180;
+      //const {queryByTestId} = render(<CalculateCenter places={places}/>);
+      const a1 = Math.sin(Δφ_1/2) * Math.sin(Δφ_1/2) +
+                Math.cos(φ1_1) * Math.cos(φ2_1) *
+                Math.sin(Δλ_1/2) * Math.sin(Δλ_1/2);
+      const c1 = 2 * Math.atan2(Math.sqrt(a1), Math.sqrt(1-a1));
+
+      const d1 = R * c1; // in metres
+
+      const φ1_2 = my_mdpt[0] * Math.PI/180; 
+      const φ2_2 = places[1].lat * Math.PI/180;
+      const Δφ_2 = (places[1].lat-my_mdpt[0]) * Math.PI/180;
+      const Δλ_2 = (places[1].lng-my_mdpt[1]) * Math.PI/180;
+      //const {queryByTestId} = render(<CalculateCenter places={places}/>);
+      const a2 = Math.sin(Δφ_2/2) * Math.sin(Δφ_2/2) +
+                Math.cos(φ1_2) * Math.cos(φ2_2) *
+                Math.sin(Δλ_2/2) * Math.sin(Δλ_2/2);
+      const c2 = 2 * Math.atan2(Math.sqrt(a2), Math.sqrt(1-a2));
+
+      const d2 = R * c2; // in metres
       
-      // const input = queryByTestId("calculateButton");
+
+      console.log(my_mdpt)
+
+      //const dist_1 = [(Math.PI/180 *my_mdpt[0] + places[0].lat ) / 2, (my_mdpt[1] + places[0].lng) / 2]
+      //const dist_2 = [(my_mdpt[0] + places[1].lat ) / 2, (my_mdpt[1] + places[1].lng) / 2]
+
+
+      console.log(d1)
+      console.log(d2)
+
+      expect(d1).toBeCloseTo(d2,5)
       // expect(input).toBeFalsy();
+      
+
+      
     })
 
     it("After the Midpoint is calculated from two points with the same location, check that the Midpoint is also that location", () => {
